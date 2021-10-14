064bd52147de20073cce1be8e453660f36059db8
Agnes <agnes.maekuusk@khk.ee>
2021-10-14 09:04:42 +0300

    Teksti väljastamine konsooli ja kommenteerimine

---
+++ app.js
@@ -0,0 +1,12 @@
+//üherealine kommentaar
+/*mitmerealine kommentaar*/
+//tektsi väljastamine
+
+
+
+console.log("Tere maailm")
+console.log('See on teine rida')
+console.log('See ' + 'on ' + 'liitlause')
+
+
+
